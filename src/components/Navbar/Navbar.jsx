a28a2a9d5a76701da9a3f63f559806b4787d3790
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-03-27 11:52:11 +0000

    created navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,97 @@
+// import React from 'react'
+import Logo from '../../assets/website/logo.png';
+import { FaCaretDown } from 'react-icons/fa';
+import { FaCartShopping } from 'react-icons/fa6';
+
+const Menu = [
+  {
+    id: 1,
+    name: 'Home',
+    link: '/',
+  },
+  {
+    id: 2,
+    name: 'Best Sellers',
+    link: '/#services',
+  },
+];
+
+const DropdownLinks = [
+  {
+    name: 'Trending Books',
+    link: '/#',
+  },
+  {
+    name: 'Best Selling',
+    link: '/#',
+  },
+  {
+    name: 'Authors',
+    link: '/#',
+  },
+];
+const Navbar = () => {
+  return (
+    <>
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <div className="container py-3 sm:py-0">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-4">
+                <img src={Logo} alt="Logo" className="w-10" />
+                Books
+              </a>
+            </div>
+            {/* menu section */}
+            <div className=" flex items-center justify-between gap-4 font-semibold">
+              <ul className="hidden sm:flex items-center gap-4">
+                {Menu.map((menu) => (
+                  <li key={menu.id}>
+                    <a
+                      href={menu.link}
+                      className="inline-block py-4 px-4 hover:text-primary duration-200"
+                    >
+                      {menu.name}
+                    </a>
+                  </li>
+                ))}
+                {/* dropdown section */}
+                <li className="group relative cursor-pointer">
+                  <a
+                    href="/#home"
+                    className="flex h-[72px] items-center gap-[2px]"
+                  >
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="transition duration-300 group-hover:rotate-180" />
+                    </span>
+                  </a>
+                  <div className="absolute -left-9 z-[9999] hidden w-[150px] rounded-md bg-white p-2 text-black group-hover:block shadow-md ">
+                    <ul className="space-y-3">
+                      {DropdownLinks.map((data) => (
+                        <li key={data.name}>
+                          <a
+                            href={data.link}
+                            className="inline-block w-full rounded-md p-2 hover:bg-primary/10"
+                          >
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+              <button className=" bg-gradient-to-r from-primary to to-secondary px-4 py-1 rounded-full flex items-center gap-3">
+                Order
+                <FaCartShopping className='text-xl  drop-shadow-sm cursor-pointer hover:scale-175 duration-300' />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
