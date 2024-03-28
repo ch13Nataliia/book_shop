4bb7edde5c5c05911ae674e19513e07c87f858a9
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-03-28 13:26:20 +0000

    created banner appStore and playStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 
+import AppStoreBanner from './components/AppStoreBanner/AppStoreBanner';
 import Banner from './components/Banner/Banner';
 import BestBooks from './components/BestBooks/BestBooks';
 import Hero from './components/Hero/Hero';
@@ -7,11 +8,12 @@ import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar />
       <Hero />
       <BestBooks />
       <Banner />
+      <AppStoreBanner />
     </div>
   );
 };
---
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -0,0 +1,53 @@
+// import React from 'react'
+import BoardImg from '../../assets/website/board.png';
+import AppStoreImg from '../../assets/website/app_store.png';
+import PlayStoreImg from '../../assets/website/play_store.png';
+
+const bannerImg = {
+  backgroundImage: `url(${BoardImg})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  width: '100%',
+  height: '100%',
+};
+const AppStoreBanner = () => {
+  return (
+    <>
+      <div
+        className="bg-gray-100 dark:bg-gray-600 text-white py-10"
+        style={bannerImg}
+      >
+        <div className="container">
+          <div className='space-y-6 max-w-xl mx-auto'>
+            <h1 className="text-2xl text-center sm:text-4xl font-semibold">
+              Read Books at your fingertips
+            </h1>
+            <div className="flex flex-wrap justify-center items-center gap-4">
+              <a href="#">
+                <img
+                  src={AppStoreImg}
+                  alt="appStore"
+                  className="
+                max-w-[150px] sm:mx-w-[120px] md:max-w-[200px]
+                "
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={PlayStoreImg}
+                  alt="playStore"
+                  className="
+                max-w-[150px] sm:mx-w-[120px] md:max-w-[200px]
+                "
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AppStoreBanner;
